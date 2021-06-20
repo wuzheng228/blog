bcbb8bcccf277150dfdc7038cee32524a48ec350
pythonFCG <5120009+pythonFCGa@user.noreply.gitee.com>
2021-06-20 22:15:47 +0800

    feat: Model层增加domain包用于自定义查询

--- src/main/java/com/zzspace/blog/dao/BlogDAO.java
+++ src/main/java/com/zzspace/blog/dao/BlogDAO.java
@@ -12,6 +12,6 @@ import java.util.List;
 @Mapper
 public interface BlogDAO {
 
-    @Select("select * from tb_blog")
+    @Select("select * from tb_blog b join tb_blog_tag tb JOIN tb_tag t where tb.blog_id = b.id AND tb.tag_id = t.id;")
     List<BlogDO> listBlog();
 }
